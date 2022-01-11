7a05f595e2503c1ceba0d5d54875740142810767
Willian Antunes <willian.lima.antunes@gmail.com>
2022-01-11 18:45:01 -0300

    feat(product-b): add boilerplate project
    This is the result of the command "sh create-project.sh ProductBee"

---
+++ product-b-regular-web-app/src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductBee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
